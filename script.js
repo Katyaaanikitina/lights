4a58131aaada9c850d2b8e5a820e1bedbffbc5f6
Katya Nikitina <katyanikitina16@gmail.com>
2022-08-19 10:40:45 +0200

    Created function 'changeColorPalette'
        *created function 'changeColorPalette' that changes the color
          palette of lights, the function uses 3 other functions:
           -turnOnBasicLights
           -turnOnBrightLights
           -turnOnVioletLights
        *added one event listener to 'Control-panel__setting-bar_color',
          which uses 'event.target' to get the button that was clicked and
          due to this chooses needed function from those three mentioned
          above
        *changed function 'turnOnLights'. Now it turns on lights in color
          palette  that is checked.
        *changed function 'turnOffLights'. Now with pusing 'off' button:
           -disapeares lights glow
           -color and background of all bulbs change to topic color
        *changed colorising of lights. Now they are colored only with JS,
          not SASS

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+
 // Getting needed sizes to culculate the number of needed bulbs
 const windowWidth = document.documentElement.clientWidth;
 const bulbWidth = parseInt(window.getComputedStyle(document.getElementsByClassName('Bulb')[0]).width);
 const wireWidth = parseInt(window.getComputedStyle(document.getElementsByClassName('Bulb')[0]).marginRight);
 const numberOfLamps = Math.floor(windowWidth / (bulbWidth + wireWidth)) - 1;
+
 
 // Creating bulbs due to the length of the window
 for (let i=0; i <= numberOfLamps; i++) {
@@ -12,19 +14,24 @@ for (let i=0; i <= numberOfLamps; i++) {
 }
 
 
-
 // Getting needed bulbs to color in particular shade
 
 const FirstBulbsAll = Array.from(document.querySelectorAll('#lights :nth-child(4n+1)'));
 const SecondBulbsAll = Array.from(document.querySelectorAll('#lights :nth-child(4n-2)'));
 const ThirdBulbsAll = Array.from(document.querySelectorAll('#lights :nth-child(4n+3)'));
 const FourthBulbsAll = Array.from(document.querySelectorAll('#lights :nth-child(4n+4)'));
+const colorChangingPanel = document.getElementsByClassName('Control-panel__setting-bar_color')[0];
 
+const basicColorPalette = document.getElementById('basic');
+const brightColorPalette = document.getElementById('bright');
+const violetColorPalette = document.getElementById('violet');
+
+
+//Functions to turn on and turn off lights
 function turnOnLights() {
-    FirstBulbsAll.forEach((bulb) => bulb.classList.add('Bulb__first'));
-    SecondBulbsAll.forEach((bulb) => bulb.classList.add('Bulb__second'));
-    ThirdBulbsAll.forEach((bulb) => bulb.classList.add('Bulb__third'));
-    FourthBulbsAll.forEach((bulb) => bulb.classList.add('Bulb__fourth'));
+    if(basicColorPalette.checked) turnOnBasicLights();
+    if(brightColorPalette.checked) turnOnBrightLights();
+    if(violetColorPalette.checked) turnOnVioletLights();
 }
 
 function turnOffLights() {
@@ -32,25 +39,29 @@ function turnOffLights() {
     SecondBulbsAll.forEach((bulb) => bulb.classList.remove('Bulb__second'));
     ThirdBulbsAll.forEach((bulb) => bulb.classList.remove('Bulb__third'));
     FourthBulbsAll.forEach((bulb) => bulb.classList.remove('Bulb__fourth'));
+
+    bulbs.forEach((bulb) => bulb.style.background = '#00a572');
+    bulbs.forEach((bulb) => bulb.style.color = '#00a572');
 }
 
 
-
 // Funtions for opening and closing setting bar
-// function openSettingsBar() {
-//     document.getElementsByClassName('Control-panel__button_settings')[0].style.display = 'none';
-//     document.getElementsByClassName('Control-panel__setting-bar')[0].style.display = 'block';
-// }
+function openSettingsBar() {
+    document.getElementsByClassName('Control-panel__button_settings')[0].style.display = 'none';
+    document.getElementsByClassName('Control-panel__setting-bar')[0].style.display = 'block';
+}
 
-// function closeSettingsBar() {
-//     document.getElementsByClassName('Control-panel__button_settings')[0].style.display = 'block';
-//     document.getElementsByClassName('Control-panel__setting-bar')[0].style.display = 'none';
-// }
+function closeSettingsBar() {
+    document.getElementsByClassName('Control-panel__button_settings')[0].style.display = 'block';
+    document.getElementsByClassName('Control-panel__setting-bar')[0].style.display = 'none';
+}
 
 
 
 //Event listener for changing the shape of bulbs 
 const shapeChangingPanel = document.getElementsByClassName('Control-panel__setting-bar_shape')[0];
+
+const bulbs = Array.from(document.getElementsByClassName('Bulb'));
 
 shapeChangingPanel.onclick = function(event) {
     const shapeButton = event.target.closest('label');
@@ -60,8 +71,6 @@ shapeChangingPanel.onclick = function(event) {
 }
 
 function changeShape(clickedButton) {
-    const bulbs = Array.from(document.getElementsByClassName('Bulb'));
-
     if(clickedButton.innerHTML == 'circle') {
         bulbs.forEach(function (bulb) {
             bulb.classList.remove('Bulb__square', 'Bulb__oval');
@@ -81,4 +90,124 @@ function changeShape(clickedButton) {
             bulb.classList.remove('Bulb__square', 'Bulb__circle');
         });
     }
+}
+
+
+//Event listener for changing the color of bulbs 
+colorChangingPanel.onclick = function(event) {
+    const colorButton = event.target.closest('input');
+    if(!colorButton) return;
+
+    changeColorPalette(colorButton);
+}
+
+function changeColorPalette(clickedButton) {
+    let id = clickedButton.id;
+    switch (id) {
+        case ('bright'): turnOnBrightLights();
+        break;
+
+        case ('violet'): turnOnVioletLights();
+        break;
+
+        default: turnOnBasicLights();
+    }
+}
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function turnOnBasicLights() {
+    FirstBulbsAll.forEach((bulb) => {
+        bulb.classList.add('Bulb__first');
+        bulb.style.background = '#E3170AFF';
+        bulb.style.color = '#E3170AFF';
+    });
+    
+    SecondBulbsAll.forEach((bulb) => {
+        bulb.classList.add('Bulb__second');
+        bulb.style.background = '#A9E5BBFF';
+        bulb.style.color = '#A9E5BBFF';
+    });
+    
+    ThirdBulbsAll.forEach((bulb) => {
+        bulb.classList.add('Bulb__third');
+        bulb.style.background = '#FCF6B1FF';
+        bulb.style.color = '#FCF6B1FF';
+    });
+    
+    FourthBulbsAll.forEach((bulb) => {
+        bulb.classList.add('Bulb__fourth');
+        bulb.style.background = '#F7B32BFF';
+        bulb.style.color = '#F7B32BFF';
+    });
+}
+    
+function turnOnBrightLights() {
+    FirstBulbsAll.forEach((bulb) => {
+        bulb.classList.add('Bulb__first');
+        bulb.style.background = '#26dfd0';
+        bulb.style.color = '#26dfd0';
+    });
+    
+    SecondBulbsAll.forEach((bulb) => {
+        bulb.classList.add('Bulb__second');
+        bulb.style.background = '#b8ee30';
+        bulb.style.color = '#b8ee30';
+    });
+    
+    ThirdBulbsAll.forEach((bulb) => {
+        bulb.classList.add('Bulb__third');
+        bulb.style.background = '#f62aa0';
+        bulb.style.color = '#f62aa0';
+    });
+    
+    FourthBulbsAll.forEach((bulb) => {
+        bulb.classList.add('Bulb__fourth');
+        bulb.style.background = '#f9d030';
+        bulb.style.color = '#f9d030';
+    });
+}
+    
+function turnOnVioletLights() {
+    FirstBulbsAll.forEach((bulb) => {
+        bulb.classList.add('Bulb__first');
+        bulb.style.background = '#ffc36d';
+        bulb.style.color = '#ffc36d';
+    });
+    
+    SecondBulbsAll.forEach((bulb) => {
+        bulb.classList.add('Bulb__second');
+        bulb.style.background = '#fe6f9b';
+        bulb.style.color = '#fe6f9b';
+    });
+    
+    ThirdBulbsAll.forEach((bulb) => {
+        bulb.classList.add('Bulb__third');
+        bulb.style.background = '#c64ab3';
+        bulb.style.color = '#c64ab3';
+    });
+    
+    FourthBulbsAll.forEach((bulb) => {
+        bulb.classList.add('Bulb__fourth');
+        bulb.style.background = '#7b50b9';
+        bulb.style.color = '#7b50b9';
+    });
 }
